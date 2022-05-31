0a71df985efc578771468448bea9e08f1be8d0c1
sardorbekzokirov <45316437+sardorick@users.noreply.github.com>
2022-05-31 15:27:04 +0200

    Backup

--- Streamlit.py
+++ Streamlit.py
@@ -11,6 +11,11 @@ from torchvision import datasets, transforms
 import time
 from torch.utils.data import DataLoader, Dataset
 import data_handler as dh
+from backup_from_peter.model_s import Classifier_Model_1, Classifier_Model_2
+from backup_from_peter.functions_s import imshow, view_classify
+import matplotlib.pyplot as plt
+
+
 
 with st.sidebar:
     menu = option_menu(menu_title='Contents',
@@ -22,11 +27,11 @@ with st.sidebar:
 if menu == 'Home':
     st.title('Clothing image classifier')
     st.markdown("Created by: _Peter, Sardorbek_")
-    image_1 = Image.open("Ralph-Lauren-Logo.png")
+    image_1 = Image.open("streamlit_images/Ralph-Lauren-Logo.png")
     st.image(image_1)
     st.markdown('We have used the Fashion MNIST dataset to create a Neural Network that can classify clothing types in this project.')
     st.markdown('The dataset consists of 60,000 28x28 Greyscale images for training and 10,000 images for test sets. ')
-    image_2 = Image.open('unnamed-chunk-8-1.png')
+    image_2 = Image.open('streamlit_images/unnamed-chunk-8-1.png')
     st.image(image_2)
     st.markdown('As the project included some extra challenges, we have tried to create a model with high accuracy and that can classify other normal clothing images.')
 
@@ -34,3 +39,26 @@ else:
     st.title('Clothing classifier model')
     st.markdown('Please upload an image')
     uploaded_file = st.file_uploader('Choose a clothing photo', accept_multiple_files=False)
+    trained_model = torch.load("backup_from_peter/trained_model_S")
+    model_state = trained_model['model_state']
+    model = Classifier_Model_1(28*28)
+    model.load_state_dict(model_state)
+
+
+
+    transform_test   = transforms.Compose([  transforms.Grayscale(), transforms.Resize((28, 28)),  transforms.ToTensor(), transforms.Normalize(mean=[0.5], std=[0.5])  ])
+    dataset_test     = datasets.FashionMNIST(root='data_validate', download=True, train=False, transform=transform_test)
+    dataloader_test  = DataLoader(dataset=dataset_test,  batch_size=64, shuffle=True)
+
+    data_iter = iter(dataloader_test)
+    images, labels = data_iter.next()
+
+
+    check_image = images[1]
+    check_image_flatten = check_image.view(check_image.shape[0], -1)
+    prediction = model.forward(check_image_flatten)
+
+
+    view_classify(check_image, prediction, version='Fashion')
+    plt.show()
+    
---
+++ backup_from_peter/prediction_s.py
@@ -0,0 +1,119 @@
+
+from tabnanny import check
+import torch 
+import torch.nn as nn 
+from model_s import Classifier_Model_1, Classifier_Model_2
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt 
+
+
+from functions_s import imshow, view_classify 
+
+
+
+
+
+trained_model = torch.load('backup_from_peter/trained_model_S')
+
+model_state = trained_model['model_state']
+
+model = Classifier_Model_1(28*28)
+model.load_state_dict(model_state)
+
+
+
+transform_test   = transforms.Compose([  transforms.Grayscale(), transforms.Resize((28, 28)),  transforms.ToTensor(), transforms.Normalize(mean=[0.5], std=[0.5])  ])
+dataset_test     = datasets.FashionMNIST(root='data_validate', download=True, train=False, transform=transform_test)
+dataloader_test  = DataLoader(dataset=dataset_test,  batch_size=64, shuffle=True)
+
+data_iter = iter(dataloader_test)
+images, labels = data_iter.next()
+
+
+check_image = images[1]
+check_image_flatten = check_image.view(check_image.shape[0], -1)
+prediction = model.forward(check_image_flatten)
+
+
+view_classify(check_image, prediction, version='Fashion')
+plt.show()
+
+
+
+
+###############################################          REAL DATA    ##################################################
+
+# processed_image = datasets.ImageFolder(root="C:/Users/Omistaja/Desktop/image_folder", transform=transform_test)
+# dataloader_test  = DataLoader(dataset=processed_image,  batch_size=2, shuffle=True)
+
+# data_iter = iter(dataloader_test)
+# images, labels = data_iter.next()
+
+
+
+# check_image = images[0]
+
+# check_image_flatten = check_image.view(check_image.shape[0], -1)
+# prediction = model.forward(check_image_flatten)
+
+
+# view_classify(check_image, prediction, version='Fashion')
+# plt.show()
+
+#########################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from PIL import Image 
+
+# image1 = Image.open("C:/Users/Omistaja/Desktop/image_folder/Dress/dress_1.jpg")
+# image_preprocess =  transforms.Compose([  transforms.Grayscale(), transforms.Resize((28, 28))  ])
+# image1 = image_preprocess(image1)
+
+# image2 = transform_test(image1)
+
+
+
+# image2 = image2.view(image2.shape[0], -1)
+# print(image2.shape)
+
+# prediction = model.forward(image2)
+
+
+# view_classify(image1, prediction, version='Fashion')
+# plt.show()
+
+
+
